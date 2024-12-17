e4f3ebda38fdcea67d9faf0db2530775a7e4ffe2
sptcnl <sptcnl@gmail.com>
2024-12-17 19:14:36 +0900

    test: 프린트 추가

--- apps/diaries/serializers.py
+++ apps/diaries/serializers.py
@@ -125,6 +125,7 @@ class AiDiaryWriteSerializer(serializers.ModelSerializer):
         if not is_exists:
             raise serializers.ValidationError('분석할 사진이 저장되지 않았습니다.')
         images = DiaryImage.objects.filter(ymd=str(ymd), username=request.user.username).first()
+        print(images)
         filename1 = images.values('image')
         filename2 = list(filename1)[0]['image']
         content = genarate_ai_diary(filename2, moods, hashtags_data)
