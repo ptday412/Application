455ae335e1b85a9e0bcee1efa5d727828e36f9dc
sptcnl <sptcnl@gmail.com>
2024-12-19 22:42:19 +0900

    test: data 말고 context에 넣어서 보내기

--- apps/diaries/views.py
+++ apps/diaries/views.py
@@ -67,7 +67,7 @@ def get_or_create_weekly_sentiments(request, year, month, weekstarts):
 
                 is_exists = Statistics.objects.filter(user=request.user, week_start=date_obj).exists()
                 if not is_exists and today > date_obj:
-                    serializer = AiStatisticSerializer(data={'week_start': week_start}, context={'request': request})
+                    serializer = AiStatisticSerializer(context={'request': request, 'week_start': week_start}, )
                     serializer.is_valid(raise_exception=True)
                     serializer.save()
                     print('>>>>>>>>>>>>>>> Saved:', serializer.data)
