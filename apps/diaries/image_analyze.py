cc9e108f13cef87bef49d52d5790435b06c08421
sptcnl <sptcnl@gmail.com>
2025-01-12 20:03:55 +0900

    test: 프린트 추가

--- apps/diaries/image_analyze.py
+++ apps/diaries/image_analyze.py
@@ -47,8 +47,11 @@ def genarate_ai_diary(images, moods, hashtags):
     bucket_name = 'test-kilolog'
     #request에서 값 가져오기
     tmp_object_key = images #예시: https://test-kilolog.s3.ap-northeast-2.amazonaws.com/youngkyu/2024-12-15/test_image.jpg
+    print('>>>>>>>>>>>>>>>>>>>tmp_object_key: ', tmp_object_key)
     object_key = tmp_object_key[1:].split('.com/')[1] #tmp_object_key.split('.com')은 이런 값으로 변경됨. 이 중 키 부분인 인덱스 1의 값을 키로 사용['https://test-kilolog.s3.ap-northeast-2.amazonaws', '/youngkyu/2024-12-15/test_image.jpg']
+    print('>>>>>>>>>>>>>>>>>>>object_key: ', object_key)
     extension_name = object_key.split('/')[-1].split('.')[1]
+    print('>>>>>>>>>>>>>>>>>>>extension_name: ', extension_name)
     extension = extension_discriminator(extension_name)
     emotion = moods
     tmp_keywords = hashtags #'키워드1, 키워드2, 키워드3'와 같은 꼴
